7ba0a5ab6fcbe3f2572e49cda1bf5048958bf87c
pjbielec <przemyslaw.bielec@core3.pl>
2018-09-26 12:11:42 +0200

    Initial commit

---
+++ src/main/java/javaworldwebflux/JavaworldWebfluxApplication.java
@@ -0,0 +1,12 @@
+package javaworldwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaworldWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaworldWebfluxApplication.class, args);
+	}
+}
